1c81fbc316afd3f3ce0ade1b54f6063b1a0f1897
bhankey <sergeysh2001@gmail.com>
2021-11-15 00:22:39 +0300

    Task1

---
+++ Day1/Task1/Animal.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace DefaultNamespace
+{
+    public class Animal: IComparable<Animal>
+    {
+        private int _age;
+        private string _name;
+
+        public Animal(int age, string name)
+        {
+            _age = age;
+            _name = name;
+        }
+
+        public int Age
+        {
+            get => _age;
+            set => _age = value;
+        }
+
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        public override string ToString()
+        {
+            return $"animal with name {_name} and age {_age}";
+        }
+
+        // Old CompareTo
+        //
+        // public int CompareTo(object obj)
+        // {
+        //     if (obj == null)
+        //     {
+        //         return 1;
+        //     }
+        //
+        //     var animal = obj as Animal;
+        //     if (animal != null)
+        //     {
+        //         return this._age.CompareTo(animal.Age);
+        //     }
+        //     
+        //     throw new ArgumentException("Object is not Animal");
+        // }
+        
+        public int CompareTo(Animal? animal)
+        {
+            if (animal == null)
+            {
+                return 1;
+            }
+            
+            return this._age.CompareTo(animal.Age);
+        }
+    }
+}
---
+++ Day1/Task1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DefaultNamespace;
+
+namespace Task1
+{
+    class Program
+    {
+        static void ArrayListTest()
+        {
+            ArrayList animalList = new ArrayList();
+
+            animalList.Add(new Animal(5, "coshka"));
+            animalList.Add(new Animal(10, "sobashka"));
+            animalList.Add(new Animal(1, "kriska"));
+
+            foreach (var animal in animalList)
+            {
+                Console.WriteLine(animal);
+            }
+            
+            animalList.Sort(); // Failed to compare elements
+            Console.WriteLine("sorted");
+            foreach (var animal in animalList)
+            {
+                Console.WriteLine(animal);
+            }
+        }
+        
+        static void ListTest()
+        {
+            List<Animal> animalList = new List<Animal>();
+
+            animalList.Add(new Animal(5, "coshka"));
+            animalList.Add(new Animal(10, "sobashka"));
+            animalList.Add(new Animal(1, "kriska"));
+
+            foreach (var animal in animalList)
+            {
+                Console.WriteLine(animal);
+            }
+            
+            animalList.Sort(); // Failed to compare elements
+            Console.WriteLine("sorted");
+            foreach (var animal in animalList)
+            {
+                Console.WriteLine(animal);
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            ArrayListTest(); // With IComparable<> didn't work
+            
+            Console.WriteLine();
+            ListTest();
+        }
+    }
+}
