22b499f8924eb224921a97c48c5eea865972b238
bhankey <sergeysh2001@gmail.com>
2021-11-14 23:50:02 +0300

    Task4 Done

---
+++ Day1/Task4/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task4
+{
+
+    
+    class Program
+    {
+        private static int _countOfIteration = 10000000;
+        
+        static long ArrayListIntTime()
+        {
+            ArrayList list = new ArrayList();
+            
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            
+            for (var i = 0; i < _countOfIteration; ++i)
+            {
+                list.Add(i);
+            }
+
+            for (var i = 0; i < _countOfIteration; ++i)
+            {
+                _ = list.GetRange(i, 1);
+            }
+            
+            watch.Stop();
+
+            return watch.ElapsedMilliseconds;
+        }
+        
+        static long ListIntTime()
+        { 
+            var list = new List<int>();
+            
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            
+            for (var i = 0; i < _countOfIteration; ++i)
+            {
+                list.Add(i);
+            }
+
+            for (var i = 0; i < _countOfIteration; ++i)
+            {
+                _ = list.GetRange(i, 1);
+            }
+            
+            watch.Stop();
+
+            return watch.ElapsedMilliseconds;
+        }
+        
+        static long ArrayListStringTime()
+        {
+            ArrayList list = new ArrayList();
+            
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            string someString = "123455";
+            for (var i = 0; i < _countOfIteration; ++i)
+            { ;
+                list.Add(someString);
+            }
+
+            for (var i = 0; i < _countOfIteration; ++i)
+            {
+                _ = list.GetRange(i, 1);
+            }
+            
+            watch.Stop();
+
+            return watch.ElapsedMilliseconds;
+        }
+        
+        static long ListStringTime()
+        { 
+            var list = new List<string>();
+            
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            string someString = "123455";
+            for (var i = 0; i < _countOfIteration; ++i)
+            {
+                list.Add(someString);
+            }
+
+            for (var i = 0; i < _countOfIteration; ++i)
+            {
+                _ = list.GetRange(i, 1);
+            }
+            
+            watch.Stop();
+
+            return watch.ElapsedMilliseconds;
+        }
+        
+        static void Main(string[] args)
+        {
+            System.GC.Collect();
+            Console.WriteLine($"Time of ArrayList With int in milliseconds {ArrayListIntTime()}");
+            System.GC.Collect();
+            Console.WriteLine($"Time of List With int in milliseconds {ListIntTime()}");
+            System.GC.Collect();
+            Console.WriteLine($"Time of ArrayList With string in milliseconds {ArrayListStringTime()}");
+            System.GC.Collect();
+            Console.WriteLine($"Time of List With int in milliseconds {ListStringTime()}");
+        }
+    }
+}
