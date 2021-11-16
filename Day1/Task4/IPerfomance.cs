942895f44a1223ed30d86bd04c891cd2a78e1974
bhankey <sergeysh2001@gmail.com>
2021-11-17 02:26:48 +0300

    Сделал задание по оч странному ТЗ, вообще хз прям

---
+++ Day1/Task4/IPerfomance.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace DefaultNamespace
+{
+    public interface IPerformance
+    {
+        public static int CountOfIteration { get; } = 10000000;
+
+        public void Check<TContainer, TArg>(TContainer c, TArg e)
+            where TContainer : IList, new();
+    }
+}
