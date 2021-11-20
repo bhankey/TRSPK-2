2e310b1bbf6117c63f776974d2486a9eb39b958b
bhankey <sergeysh2001@gmail.com>
2021-11-20 03:50:07 +0300

    [TRSPK-20] Улучшил чуть класс. Он все равно нуждается в доработке

--- LucasKanade/LucasKanade/Program.cs
+++ LucasKanade/LucasKanade/Program.cs
@@ -3,6 +3,7 @@ using System.IO;
 using MediaToolkit;
 using MediaToolkit.Model;
 using MediaToolkit.Options;
+using SixLabors.ImageSharp;
 
 namespace LucasKanade
 {
@@ -10,8 +11,16 @@ namespace LucasKanade
     {
         static void Main(string[] args)
         {
-            var splitter = new VideoSplitterV2("./video/movementFirst.mp4");
-            splitter.Parse();
+            var splitter = new VideoSplitter("./video/movementFirst.mp4");
+            
+            splitter.LoadVideo();
+            for (int i = 0; i < 500; i++)
+            {
+                _ = splitter.TryGetNextFrame(out var frame);
+                frame.SaveAsPng($"C:\\Users\\Sergey\\RiderProjects\\TRSPK-2\\LucasKanade\\photos\\test{i}.png");
+            }
+
+            
         }
     }
 }
--- LucasKanade/LucasKanade/VideoSplitter.cs
+++ LucasKanade/LucasKanade/VideoSplitter.cs
@@ -1,42 +1,83 @@
 ﻿using System;
-using MediaToolkit;
-using MediaToolkit.Model;
-using MediaToolkit.Options;
+using System.Collections.Generic;
+using System.Drawing;
+using FFMediaToolkit.Decoding;
+using FFMediaToolkit.Graphics;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using Image = SixLabors.ImageSharp.Image;
 
 namespace LucasKanade
 {
+    // Чтобы этот класс заработал надо убить кучу времени, ибо доки к либам очень плохие на C#.
+    // Необходимо, чтобы были установлены нужные пакеты и наличие папки /runtimes/win-x64/native
+    // (P.S в доке про эту папка ничего нет, слава животворящему дебагу)
+    // Если что-то не работает, то поставьте сборку только для x64
+    
+    // TODO можно доработать класс для работы с потоком
     public class VideoSplitter
     {
         private string _pathToVideo;
 
-        private string _pathToOutPut;
+        private MediaFile _file;
 
-        public VideoSplitter(string pathToVideo, string pathToOutPut = "./tmp")
+        private bool _isLoaded = false;
+        public VideoSplitter(string pathToVideo)
         {
             _pathToVideo = pathToVideo;
-            _pathToOutPut = pathToOutPut;
+        }
+        
+        public void Clean()
+        {
+            _file.Dispose();
+            _isLoaded = false;
         }
 
-        public void Parse()
+        public void LoadVideo()
         {
-            using (var engine = new Engine())
-            {
-                var video = new MediaFile {Filename = _pathToVideo};
+            _file = MediaFile.Open(_pathToVideo);
+            _isLoaded = true;
+        }
 
-                engine.GetMetadata(video);
-                var milliSecondPerCadre = video.Metadata.VideoData.Fps / 1000;
-                var i = 0d;
-                var counter = 0;
-                while (i < video.Metadata.Duration.TotalMilliseconds)
-                {
-                    var options = new ConversionOptions {Seek = TimeSpan.FromMilliseconds(i)};
-                    var outputFile = new MediaFile
-                        {Filename = string.Format("{0}\\image-{1}.jpeg", _pathToOutPut, counter)};
-                    engine.GetThumbnail(video, outputFile, options);
-                    i += milliSecondPerCadre;
-                    ++counter;
-                }
+        public bool TryGetNextFrame(out Image bitmap)
+        {
+            bitmap = default;
+            if (!_isLoaded)
+            {
+                return false;
             }
+
+            var ok = _file.Video.TryGetNextFrame(out var imageData);
+
+            if (!ok)
+            {
+                return false;
+            }
+
+            bitmap = ToBitmap(imageData);
+
+            return true;
+        }
+        public List<Image<Bgr24>> GetAllFrames()
+        {
+            if (!_isLoaded)
+            {
+                throw new Exception("Video is not loaded");
+            }
+            
+            var images = new List<Image<Bgr24>>();
+            while (_file.Video.TryGetNextFrame(out var imageData))
+            {
+                var image = ToBitmap(imageData);
+                images.Add(image);
+            }
+
+            return images;
+        }
+        
+        private static Image<Bgr24> ToBitmap(ImageData imageData)
+        {
+            return Image.LoadPixelData<Bgr24>(imageData.Data, imageData.ImageSize.Width, imageData.ImageSize.Height);
         }
     }
 }
