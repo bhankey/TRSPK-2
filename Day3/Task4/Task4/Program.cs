695e403b4dab0b57f5d7d8f8c64e85c73fe8618c
bhankey <sergeysh2001@gmail.com>
2021-11-23 22:32:18 +0300

    TRCPK-61

--- Day3/Task4/Task4/Program.cs
+++ Day3/Task4/Task4/Program.cs
@@ -5,67 +5,59 @@ namespace Task4
 {
     class Program
     {
-        class ThreadSafe
+        static void FirstFunction(AutoResetEvent first, AutoResetEvent second)
         {
-            static readonly object _locker = new object();
-            private static int i;
+            while (true)
+            {
+                first.WaitOne();
 
-            public static int Get()
-            {
-                lock (_locker) return i;
+                Console.Write("1");
+
+                second.Set();
             }
-            public static void Increment() { lock (_locker) i++; }
-            public static void Assign(int x)    { lock (_locker) i = x; }
         }
 
-        static void FirstFunction()
+        static void SecondFunction(AutoResetEvent second, AutoResetEvent third)
         {
             while (true)
             {
-                if (ThreadSafe.Get() == 1)
-                {
-                    Console.Write("1");
-                    ThreadSafe.Increment();
-                }
+                second.WaitOne();
+
+                Console.Write(" 2");
+
+                third.Set();
             }
         }
-        
-        static void SecondFunction()
+
+        static void ThirdFunction(AutoResetEvent third, AutoResetEvent first)
         {
+
             while (true)
             {
-                if (ThreadSafe.Get() == 2)
-                {
-                    Console.Write(" 2");
-                    ThreadSafe.Increment();
-                }
-            }
-        }
-        
-        static void ThirdFunction()
-        {
-            while (true)
-            {
-                if (ThreadSafe.Get() == 3)
-                {
-                    Console.WriteLine(" 3");
-                    ThreadSafe.Assign(1);
-                }
+                third.WaitOne();
+
+                Console.WriteLine(" 3");
+
+                first.Set();
             }
         }
             
         
         static void Main(string[] args)
         {
-            var f = new Thread(FirstFunction);
-            var s = new Thread(SecondFunction);
-            var t = new Thread(ThirdFunction);
-
-            ThreadSafe.Assign(1);
+            var event1 = new AutoResetEvent(false);
+            var event2 = new AutoResetEvent(false);
+            var event3 = new AutoResetEvent(false);
             
+            var f = new Thread(() => FirstFunction(event1, event2));
+            var s = new Thread(() => SecondFunction(event2, event3));
+            var t = new Thread(() => ThirdFunction(event3, event1));
+
             f.Start();
             s.Start();
             t.Start();
+
+            event1.Set();
 
             f.Join();
             
