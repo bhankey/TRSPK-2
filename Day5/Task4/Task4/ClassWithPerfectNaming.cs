ebabe0cfeeefa7132550bfa2fccd784c9fbdf2a6
bhankey <sergeysh2001@gmail.com>
2021-12-11 00:10:38 +0300

    Task4

---
+++ Day5/Task4/Task4/ClassWithPerfectNaming.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+
+namespace Task4
+{
+    public class ClassWithPerfectNaming: IDisposable
+    {
+        private string F;
+        private string S;
+
+        private FileStream FStream;
+        private FileStream SStream;
+        
+        private bool disposed = false;
+        public ClassWithPerfectNaming(string f, string s)
+        {
+            (F, S) = (f, s);
+        }
+
+        public void Open()
+        {
+            try
+            {
+                FStream = File.OpenRead(F);
+                SStream = File.OpenWrite(S);
+            }
+            catch (Exception e)
+            {
+                FStream?.Close();
+                SStream?.Close();
+                throw;
+            }
+        }
+
+        public void Work()
+        {
+            using (var streamReader = new StreamReader(FStream))
+            using (var streamWriter = new StreamWriter(SStream))
+            {
+                while (true)
+                {
+                    var line = streamReader?.ReadLine();
+                    if (line == null)
+                    {
+                        break;
+                    }
+                    
+                    streamWriter.WriteLine(line);
+                }
+            }
+        }
+
+        public void Close()
+        {
+            Dispose(true);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    F = "";
+                    S = "";
+                }
+                FStream?.Dispose();
+                SStream?.Dispose();
+                
+                disposed = true;
+            }
+        }
+        
+        ~ClassWithPerfectNaming()
+        {
+            Dispose(false);
+        }
+    }
+}
---
+++ Day5/Task4/Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var f = new ClassWithPerfectNaming("C:\\Users\\Sergey\\RiderProjects\\TRSPK-2\\Day5\\Task4\\l1.txt", "C:\\Users\\Sergey\\RiderProjects\\TRSPK-2\\Day5\\Task4\\l2.txt"))
+            {
+                f.Open();
+                f.Work();
+            }
+            
+            
+        }
+    }
+}
