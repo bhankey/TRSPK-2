5ea36f7e9bdfe7a575b0aeaf09167d21e9ae1d26
bhankey <sergeysh2001@gmail.com>
2021-12-16 04:33:48 +0300

    convolution!!!!!

---
+++ LucasKanade/LucasKanade/Registry.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace LucasKanade
+{
+    public static class Registry
+    {
+        private static readonly Dictionary<string, object> rDictionary = new()
+        {
+            { "gausin_filter", 1}
+        };
+
+        public static object Get(string key)
+        {
+            return rDictionary[key];
+        }
+        
+    }
+}
